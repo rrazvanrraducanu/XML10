398e4714218ddd48224139a343a2295ba95bb42f
Razvan Raducanu <rrazvan.rraducanu@gmail.com>
2018-04-13 11:47:32 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>TODO supply a title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+ <svg width="640" height="480" xmlns="http://www.w3.org/2000/svg">
+ <!-- Created with SVG-edit - http://svg-edit.googlecode.com/ -->
+ <g>
+  <title>Layer 1</title>
+  <ellipse ry="140.5" rx="137" id="svg_1" cy="214.5" cx="320" stroke-width="5" stroke="#000000" fill="#FF0000"/>
+  <ellipse ry="274" rx="467" id="svg_2" cy="983" cx="1374" stroke-width="5" stroke="#000000" fill="#FF0000"/>
+  <ellipse rx="116" id="svg_3" cy="883" cx="1376" stroke-width="5" stroke="#000000" fill="#FF0000"/>
+ </g>
+</svg>
+<math display="block">
+<mrow>
+  <mi>a</mi>
+  <mo>/</mo>
+  <mi>b</mi>
+</mrow>
+</math>
+       
+    </body>
+</html>
